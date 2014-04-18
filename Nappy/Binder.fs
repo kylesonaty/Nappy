45631f29885d69ac5e25672a27d1fcfd43a41cfa
kylesonaty <kyle.sonaty@gmail.com>
2014-04-18 00:30:42 -0400

    Added XML serialization to model binder.

--- Nappy/Binder.fs
+++ Nappy/Binder.fs
@@ -1,11 +1,20 @@
 ﻿namespace Nappy
 
 open System
+open System.IO
+open System.Text
+open System.Runtime.Serialization
+open System.Xml
 open Newtonsoft.Json
 
 module Binder =
     let DeserializeJson(t:Type, content:string) =
         JsonConvert.DeserializeObject(content,t)
+
+    let DeserializeXml(t:Type, content:string) =
+        let serializer = new DataContractSerializer(t)
+        let reader = new XmlTextReader(new StringReader(content))
+        serializer.ReadObject(reader)
 
     let Bind (t:Type, content:string, contentType:ContentType) =
         match content with
@@ -13,6 +22,6 @@ module Binder =
         | c -> 
             match contentType with
                 | ContentType.Json -> DeserializeJson(t, content)
-                | ContentType.Xml  -> failwith "Not Implemented"
+                | ContentType.Xml  -> DeserializeXml(t, content)
                 | ContentType.Form -> failwith "Not Implemented"
                 | ContentType.None -> failwith "Not Implemented"
